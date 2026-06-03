368cba9b50f0b1cee482a9227352bd84e9a19a58
VerbruggeTom <tom.verbrugge@student.howest.be>
2026-06-03 13:20:06 +0200

    Move ApplicationUser to Domain, create rest of entities

---
+++ TalentFlow/TalentFlow.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentFlow.Domain.Entities;
+
+namespace TalentFlow.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
+        public ICollection<Audition> Auditions { get; set; } = new List<Audition>();
+    }
+}
---
+++ TalentFlow/TalentFlow.Domain/Entities/Audition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentFlow.Domain.Enums;
+
+namespace TalentFlow.Domain.Entities
+{
+    public class Audition
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public string? Role { get; set; }
+        public string? Language { get; set; }
+        public string? ClientName { get; set; }
+        public bool CustomAuditionRequired { get; set; }
+        public AuditionStatus Status { get; set; } = AuditionStatus.Interested;
+        public DateTime? Deadline { get; set; }
+        public DateTime? SubmittedAt { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; } = null!;
+        public Guid? PlatformId { get; set; }
+        public Platform? Platform { get; set; }
+        public ICollection<AuditionFile> AuditionFiles { get; set; } = new List<AuditionFile>();
+    }
+}
